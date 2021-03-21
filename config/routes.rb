aadb72a91351a58f20858ba129f59785ed9240a2
YCChen <t5204713910@gmail.com>
2021-03-21 18:18:25 +0800

    add project

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-    authenticate :user, lambda { |u| u.admin? } do
-      mount Sidekiq::Web => '/sidekiq'
-    end
+  resources :projects
 
+  # vist sidekiq if user is admin
+  authenticate :user, lambda { |u| u.admin? } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
 
   devise_for :users
-  root to: 'home#index'
+  root to: 'projects#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
