d9d241e125aa4178832d0adbcbf3085ec37e4029
YCChen <t5204713910@gmail.com>
2021-03-23 10:59:08 +0800

    better looking in sign_in/sign_up/project_index/project_show/project_edit

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,7 @@
 class ProjectsController < ApplicationController
   before_action :set_project, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, except: [:index, :show]
+
 
   # GET /projects or /projects.json
   def index
@@ -22,6 +24,7 @@ class ProjectsController < ApplicationController
   # POST /projects or /projects.json
   def create
     @project = Project.new(project_params)
+    @project.user_id = current_user.id
 
     respond_to do |format|
       if @project.save
