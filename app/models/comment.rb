9fbf6f146eed64e6054ddae92da7d3cc5add9295
YCChen <t5204713910@gmail.com>
2021-06-09 16:13:21 +0800

    add replys to comments section

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :commentable, polymorphic: true # with only one line, all model can have it
+  has_many   :replies, class_name: 'Comment', foreign_key: :commentable_id, dependent: :destroy
 end
