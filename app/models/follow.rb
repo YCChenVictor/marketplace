3405e031aa60bf19cc6200f2cdad6cc70f4b156a
YCChen <t5204713910@gmail.com>
2021-07-15 17:12:42 +0800

    add follow model to record follower and followee

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,2 +1,12 @@
 class Follow < ApplicationRecord
+  belongs_to :follower, class_name: 'User'
+  belongs_to :followee, class_name: 'User'
+
+  validate :not_following_self
+  private
+    def not_following_self
+      if follower_id == followee_id
+        errors.add(:follower_id, 'You cannot follow yourself')
+      end
+    end
 end
